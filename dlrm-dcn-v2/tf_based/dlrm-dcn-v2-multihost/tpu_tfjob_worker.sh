249622aa26b11093e6ce392a3feb2ed372db690b
Sina Chavoshi <sina.chavoshi@gmail.com>
2025-10-17 20:27:29 +0000

    revert to worker/ master setup

--- dlrm-dcn-v2/tf_based/dlrm-dcn-v2-multihost/tpu_tfjob_worker.sh
+++ dlrm-dcn-v2/tf_based/dlrm-dcn-v2-multihost/tpu_tfjob_worker.sh
@@ -8,6 +8,7 @@ IFS=',' read -r -a hostnames <<<"$TPU_WORKER_HOSTNAMES"
 export TPU_HOSTNAME_OVERRIDE="${hostnames[${TPU_WORKER_ID}]}"
 
 # Set low-level driver and XLA flags discovered in the working image
+export LIBTPU_INIT_ARGS="--xla_sc_splitting_along_feature_dimension=auto --copy_with_dynamic_shape_op_output_pjrt_buffer=true"
 export TF_XLA_FLAGS="--tf_mlir_enable_mlir_bridge=true --tf_xla_sparse_core_disable_table_stacking=true --tf_mlir_enable_convert_control_to_data_outputs_pass=true --tf_mlir_enable_merge_control_flow_pass=true --tf_mlir_enable_tpu_variable_runtime_reformatting_pass=false --tf_xla_disable_full_embedding_pipelining=true"
 
 # Set PluggableDevice and TPU library paths
